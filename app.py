91ecebafa550414848f02393e77fd024fea3ad23
av2mu <anishtoomu@gmail.com>
2024-09-11 16:43:18 -0400

    changed to flask-restful using resource classes

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from database import db
 from datetime import datetime
 import os
 import logging
+from flask_restful import Api, Resource, reqparse, inputs
+from werkzeug.datastructures import FileStorage
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -49,21 +51,6 @@ def create_app():
     @app.route('/api/v1/workouts', methods=['GET'])
     def get_workouts():
         workouts = Workout.query.order_by(Workout.date.desc()).all()
-        user_profile = UserProfile.query.first()
-        weight = user_profile.weight if user_profile else 70  # Default weight if not set
-
-        workout_dicts = []
-        for workout in workouts:
-            workout_dict = workout.to_dict()
-            workout_dict['calories_burned'] = workout.calculate_calories_burned(weight)
-            workout_dicts.append(workout_dict)
-
-        return jsonify(workout_dicts)
-
-    @app.route('/api/v1/workouts/search', methods=['GET'])
-    def search_workouts():
-        query = request.args.get('q', '')
-        workouts = Workout.query.filter(Workout.route_nickname.ilike(f'%{query}%')).order_by(Workout.date.desc()).all()
         user_profile = UserProfile.query.first()
         weight = user_profile.weight if user_profile else 70  # Default weight if not set
 
@@ -139,4 +126,105 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
+    api = Api(app)
+    
+    # Request parser for workout creation
+    workout_parser = reqparse.RequestParser()
+    workout_parser.add_argument('duration', type=float, required=True, help='Duration in minutes is required')
+    workout_parser.add_argument('distance', type=float, required=True, help='Distance in miles is required')
+    workout_parser.add_argument('route_nickname', type=str, required=True, help='Route nickname is required')
+    workout_parser.add_argument('date', type=inputs.datetime_from_iso8601, required=True, help='Date in ISO8601 format is required')
+    workout_parser.add_argument('heart_rate', type=int)
+    workout_parser.add_argument('image', type=FileStorage, location='files')
+
+    # Request parser for user profile update
+    profile_parser = reqparse.RequestParser()
+    profile_parser.add_argument('weight', type=float, required=True, help='Weight in kg is required')
+
+    class WorkoutListAPI(Resource):
+        def get(self):
+            workouts = Workout.query.order_by(Workout.date.desc()).all()
+            user_profile = UserProfile.query.first()
+            weight = user_profile.weight if user_profile else 70  # Default weight if not set
+
+            workout_dicts = []
+            for workout in workouts:
+                workout_dict = workout.to_dict()
+                workout_dict['calories_burned'] = workout.calculate_calories_burned(weight)
+                workout_dicts.append(workout_dict)
+
+            return workout_dicts
+
+        def post(self):
+            args = workout_parser.parse_args()
+            
+            new_workout = Workout(
+                duration=args['duration'],
+                distance=args['distance'],
+                route_nickname=args['route_nickname'],
+                date=args['date'],
+                heart_rate=args['heart_rate']
+            )
+            
+            if args['image']:
+                image = args['image']
+                if image.filename != '':
+                    filename = secure_filename(image.filename)
+                    image_filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{filename}"
+                    image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_filename))
+                    new_workout.image_filename = image_filename
+            
+            db.session.add(new_workout)
+            db.session.commit()
+            
+            return new_workout.to_dict(), 201
+
+    class WorkoutAPI(Resource):
+        def delete(self, id):
+            workout = Workout.query.get_or_404(id)
+            db.session.delete(workout)
+            db.session.commit()
+            return '', 204
+
+    class UserProfileAPI(Resource):
+        def get(self):
+            profile = UserProfile.query.first()
+            if not profile:
+                profile = UserProfile(weight=0)
+                db.session.add(profile)
+                db.session.commit()
+            return profile.to_dict()
+
+        def put(self):
+            args = profile_parser.parse_args()
+            profile = UserProfile.query.first()
+            if not profile:
+                profile = UserProfile(weight=args['weight'])
+                db.session.add(profile)
+            else:
+                profile.weight = args['weight']
+            
+            db.session.commit()
+            return profile.to_dict()
+
+    class WorkoutSearchAPI(Resource):
+        def get(self):
+            query = request.args.get('q', '')
+            workouts = Workout.query.filter(Workout.route_nickname.ilike(f'%{query}%')).order_by(Workout.date.desc()).all()
+            user_profile = UserProfile.query.first()
+            weight = user_profile.weight if user_profile else 70  # Default weight if not set
+
+            workout_dicts = []
+            for workout in workouts:
+                workout_dict = workout.to_dict()
+                workout_dict['calories_burned'] = workout.calculate_calories_burned(weight)
+                workout_dicts.append(workout_dict)
+
+            return workout_dicts
+
+    api.add_resource(WorkoutListAPI, '/api/v1/workouts')
+    api.add_resource(WorkoutAPI, '/api/v1/workouts/<int:id>')
+    api.add_resource(UserProfileAPI, '/api/v1/profile')
+    api.add_resource(WorkoutSearchAPI, '/api/v1/workouts/search')
+
     app.run(debug=True)
